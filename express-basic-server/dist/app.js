37304b5032d75bf0aa283ea1fb9cdad735eeb3a9
Dodul01 <mhdodul299@gmail.com>
2024-11-15 18:06:39 +0600

    Basic module completed

--- express-basic-server/dist/app.js
+++ express-basic-server/dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,14 +16,77 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 // Parsers
 app.use(express_1.default.json());
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-app.post('/', (req, res) => {
+app.use(express_1.default.text());
+// Routing in express
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/courses', courseRouter);
+userRouter.post('/create-user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        sucess: true,
+        message: 'user created sucessfully.',
+        data: user
+    });
+}));
+courseRouter.post('/create-course', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        sucess: true,
+        message: 'course created sucessfully.',
+        data: course
+    });
+}));
+// middleware
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get('/', logger, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send('Hello World');
+        // res.send(something)
+    }
+    catch (error) {
+        console.log(error);
+        // res.status(400).json({ message: 'something went wrong.' })
+        next(error);
+    }
+}));
+//req param
+// app.get('/:userId/:subId', (req: Request, res: Response) => {
+//     console.log(req.params);
+//     res.send('Hello World')
+// })
+// query params 
+app.get('/users', logger, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(req.query);
+    res.json({
+        'message': 'Users api hit sucessfully.',
+        'email': `you query email ${req.query.email}`
+    });
+}));
+app.post('/', logger, (req, res) => {
     console.log(req.body);
     // res.send('got the data')
     res.json({
         'message': 'Data received successfully'
     });
+});
+app.all('*', (req, res) => {
+    res.status(400).json({
+        sucess: false,
+        message: 'Have a relax ha ha something went wong.'
+    });
+});
+// global error handler
+app.use((error, req, res, next) => {
+    console.log(error);
+    if (error) {
+        res.status(400).json({ sucess: false, message: 'something went wrong.' });
+    }
 });
 exports.default = app;
--- express-basic-server/src/app.ts
+++ express-basic-server/src/app.ts
@@ -1,19 +1,93 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 const app = express()
 
 // Parsers
 app.use(express.json())
+app.use(express.text())
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
+
+// Routing in express
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/courses', courseRouter)
+
+userRouter.post('/create-user', async (req: Request, res: Response) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        sucess: true,
+        message: 'user created sucessfully.',
+        data: user
+    })
 })
 
-app.post('/', (req: Request, res: Response) => {
+courseRouter.post('/create-course', async (req: Request, res: Response) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        sucess: true,
+        message: 'course created sucessfully.',
+        data: course
+    })
+})
+
+// middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+}
+
+app.get('/', logger, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.send('Hello World')
+        // res.send(something)
+    } catch (error) {
+        console.log(error);
+        // res.status(400).json({ message: 'something went wrong.' })
+        next(error)
+    }
+})
+
+//req param
+// app.get('/:userId/:subId', (req: Request, res: Response) => {
+//     console.log(req.params);
+
+//     res.send('Hello World')
+// })
+
+// query params 
+app.get('/users', logger, async (req: Request, res: Response) => {
+    console.log(req.query);
+    res.json({
+        'message': 'Users api hit sucessfully.',
+        'email': `you query email ${req.query.email}`
+    })
+})
+
+app.post('/', logger, (req: Request, res: Response) => {
     console.log(req.body)
     // res.send('got the data')
     res.json({
         'message': 'Data received successfully'
     })
+})
+
+app.all('*', (req: Request, res: Response) => {
+    res.status(400).json({
+        sucess: false,
+        message: 'Have a relax ha ha something went wong.'
+    })
+})
+
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(error);
+    if (error) {
+        res.status(400).json({ sucess: false, message: 'something went wrong.' })
+    }
 })
 
 export default app;
